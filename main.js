be63b16110a2bc9f0ad20ddeb4981d29a98a1b44
Vitalii <tka4uk.gt41@gmail.com>
2024-05-06 19:31:52 +0600

    new

--- main.js
+++ main.js
@@ -7,47 +7,48 @@ let allWorkKeys = []
 const audio = new Audio(`./sounds/a.wav`)
 
 for (let i = 0; i < keys.length; i++) {
-    allWorkKeys.push(keys[i].getAttribute('id'))
-    keys[i].addEventListener('mousedown', (event) => {
-        event.currentTarget.classList.add('active')
-        playSound(event.currentTarget.getAttribute('id'))
-    })
-    keys[i].addEventListener('mouseup', (event) => {
-        event.currentTarget.classList.remove('active')
-    })
-    keys[i].addEventListener('mouseout', (event) => {
-        event.currentTarget.classList.remove('active')
-    })
+  allWorkKeys.push(keys[i].getAttribute('id'))
+  keys[i].addEventListener('mousedown', (event) => {
+    event.currentTarget.classList.add('active')
+    playSound(event.currentTarget.getAttribute('id'))
+  })
+  keys[i].addEventListener('mouseup', (event) => {
+    event.currentTarget.classList.remove('active')
+  })
+  keys[i].addEventListener('mouseout', (event) => {
+    event.currentTarget.classList.remove('active')
+  })
 }
 function playSound(keyName) {
-    audio.src = `./sounds/${keyName}.wav`
-    audio.play()
+  audio.src = `./sounds/${keyName}.wav`
+  audio.play()
+  console.log('press');
 }
 
-document.addEventListener('keydown', event => {
-    if (allWorkKeys.includes(event.key)) {
-        playSound(event.key)
-        document.getElementById(event.key).classList.add('active')
-    } else {
-        console.log('another key');
-    }
+document.addEventListener('keydown', (event) => {
+  if (allWorkKeys.includes(event.key)) {
+    playSound(event.key)
+    document.getElementById(event.key).classList.add('active')
+  } else {
+    console.log('another key')
+  }
 })
-document.addEventListener('keyup', event => {
-    if (allWorkKeys.includes(event.key)) {
-        document.getElementById(event.key).classList.remove('active')
-    }
+document.addEventListener('keyup', (event) => {
+  if (allWorkKeys.includes(event.key)) {
+    document.getElementById(event.key).classList.remove('active')
+  }
 })
 
 function handleVolume(e) {
-    audio.volume = e.target.value
+  audio.volume = e.target.value
 }
 
 volumeSlider.addEventListener('input', handleVolume)
 
 const showHideKeys = () => {
-    document.querySelectorAll('.key-wrapper span').forEach(element => {
-        element.classList.toggle('display-none')
-    })
+  document.querySelectorAll('.key-wrapper span').forEach((element) => {
+    element.classList.toggle('display-none')
+  })
 }
 
 checkBox.addEventListener('click', showHideKeys)
